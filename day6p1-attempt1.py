9b6fc91a723e506bcb4b746bd800588e7d827a8d
kenshin133 <bufmouse13@gmail.com>
2023-06-03 16:15:04 -0400

    base files for day6

---
+++ day6p1-attempt1.py
@@ -0,0 +1,9 @@
+file=open("day5-input","r")
+
+temp=file.readlines()
+from day6func import *
+
+# '''example 
+#
+
+print("answers for smaples should be : \n     bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5 \n nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6 \n nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10 \n zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11")
